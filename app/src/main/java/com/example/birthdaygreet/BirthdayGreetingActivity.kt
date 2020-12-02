9dd5f5010791c036fff2eed7d9432ea123f6cf04
Saransh <saransh0701@gmail.com>
2020-12-02 19:05:21 +0530

    Fixed issue with passing text from BirthDayGreetingActivity.kt to screen by making a new variable

--- app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
@@ -2,6 +2,7 @@ package com.example.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_birthday_greeting.*
 
 class BirthdayGreetingActivity : AppCompatActivity() {
@@ -14,7 +15,9 @@ class BirthdayGreetingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
 
+        val receiver_msg = findViewById<TextView>(R.id.birthdayGreeting)
+
         val name = intent.getStringExtra(NAME_EXTRA)
-        birthdayGreeting.text = "Happy Birthday\n$name!"
+        receiver_msg.text = "Happy Birthday\n$name!"
     }
 }
